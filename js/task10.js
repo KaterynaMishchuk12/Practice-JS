f256a3e7018b322dc9d11445cdb484503663755d
ValiaStebivka <valia.stebivka2016@gmail.com>
2023-10-29 11:35:17 +0200

    add task 10

---
+++ js/task10.js
@@ -0,0 +1,36 @@
+// *? Напиши функцію конструктор User для 
+// *? створення користувача з такими властивостями:
+// *? a. userName - ім'я, рядок
+// *? b. age - вік, число
+// *? c. numbersOfPost - кількість постів, число
+// *? d. клас очікує 1 параметр - об'єкт налаштувань з однойменними властивостями
+
+// *? Додай метод getInfo(), який повертає рядок:
+// *? `Користувачеві ${ім'я} ${вік} років і в нього ${кількістьПублікацій} публікацій.`
+
+function User({userName, age, numbersOfPost}) {
+    this.userName = userName;
+    this.age = age;
+    this.numbersOfPost = numbersOfPost;
+    this.getInfo = function () {
+        const {userName, age, numbersOfPost} = this;
+        return `Користувачеві ${userName} ${age} років і в нього ${numbersOfPost} публікацій.`
+    } 
+}
+
+const polly = new User({
+    userName: "Polly",
+    age: 30,
+    numbersOfPost: 15
+});
+
+console.log(polly.getInfo())
+
+
+const vasia = new User({
+    userName: "Vasia",
+    age: 25,
+    numbersOfPost: 30
+});
+
+console.log(vasia.getInfo())
